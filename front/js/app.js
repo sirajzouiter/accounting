00bdf00a40a0ed547e18cb3ce08d01f7159b8c6d
Ferid HELALI <helaliferid@gmail.com>
2019-07-18 12:57:24 +0100

    server and xhr js

--- front/js/app.js
+++ front/js/app.js
@@ -1,3 +1,17 @@
-(function(){
-    console.log('Hello World');
+var users;
+(function () {
+    function later() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("output").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", "http://localhost:3030/users", true);
+        xhttp.send();
+    };
+
+    setTimeout(later(),10000);
 })();
+
+// XmlHTTPRequest / innerHTML / setTimeout / Git 
